07fc0dca299bf4c40008380636bf610581130b3e
Aryan1630 <35829586+Aryan1630@users.noreply.github.com>
2021-11-17 15:10:30 +0530

    master1

--- cart.js
+++ cart.js
@@ -1,10 +1,36 @@
+//cart page
 const addedUsers = document.getElementById('addedUsers');
+const removeButton = document.getElementsByClassName('remove');
+console.log(removeButton);
 
-function getAddedUsersList(){
-    var users = localStorage.getItem('users');
-    
-    let arrUsersSplit = users.split(',');
-    console.log(arrUsersSplit);
+function getUsersList(){
+    var users = [localStorage.getItem('users')];
+    // console.log(users);
+    var coupledUsers = users.match(/([0-9]+ [A-z]+, [0-9]+)/g);
+      console.log(coupledUsers);
+    var arrayLength = users.length;
+
+    for(i=0;i<arrayLength;i++){
+     var usersList = users[i];
+      var splittedUsers = usersList.split(',');
+       var names = splittedUsers.filter(function(val,idx) {
+          if(idx%2==0)
+            return val;
+        })
+       var userImages = splittedUsers.filter(function(val,idx) {
+          if(!(idx%2==0))
+            return val;
+    }) 
+        for(i=0;i<names.length&&userImages;i++){
+            document.getElementById('addedUsers').insertAdjacentHTML('afterbegin', `<div class="card"><img src="${userImages[i]}"> <h5 class="login">${names[i]}</h5><button class="remove" type='submit'>Remove button</button></div>`)
+      
+      }
+
+      
+      
 }
+}
+getUsersList();
+console.log(removeButton.length);
 
-getAddedUsersList();
+
